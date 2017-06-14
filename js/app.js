2c94a1e00ef882251e10419cd5b76f1e01b84ce1
Rico Evers <ricoevers@live.nl>
2017-06-14 12:17:13 +0200

    setup socket io

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/../build/'));
+
+app.get('/', (req, res)=>{
+    var path = require('path');
+    res.sendFile(path.resolve(__dirname + '/../html/index.html'));
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
