bd098fac2fc8172a30d3362e73587cc7f46fad59
jvdwijk <joeyvdwijk@gmail.com>
2017-06-19 10:16:09 +0200

    powerselect small update

--- js/powerSelect.js
+++ js/powerSelect.js
@@ -1,7 +1,22 @@
-var elements = ["fire","water","electricity","ground","wind","arcane"];
+let elements = ["fire", "water", "electricity", "ground", "wind", "arcane"];
 
 
-window.addEventListener("click", (e) => 
-{
-    
-}
+window.addEventListener("click", (e) => {
+    switch (e) {
+    case 1:
+        console.log("vuur");
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    default:
+        break;
+    }
+});
