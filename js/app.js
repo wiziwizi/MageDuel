242f1e64a31ac7824ff0ed994542da4360ab62bb
Rico Evers <ricoevers@live.nl>
2017-06-19 10:21:12 +0200

    app.js update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const serverPort = 3000;
 
 app.use(express.static(__dirname + '/../js/'));
 app.use(express.static(__dirname + '/../html/'));
 
 app.get('/', (req, res)=>{
-    var path = require('path');
+    let path = require('path');
     res.sendFile(path.resolve(__dirname + '/../html/index.html'));
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+io.on('connection', (socket)=>{
+  socket.on('chat message', (msg)=>{
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(serverPort, ()=>{
+    console.log('listening on *:' + serverPort);
 });
