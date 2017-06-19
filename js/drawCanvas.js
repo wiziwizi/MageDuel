a3e778a7ace5230884c4eb7982d9afb1ace07454
jvdwijk <joeyvdwijk@gmail.com>
2017-06-19 12:13:51 +0200

    pixi remove
    to be reinstalled

---
+++ js/drawCanvas.js
@@ -0,0 +1,5 @@
+/*let renderer    = PIXI.autoDetectRenderer(256,256);
+let stage       = new PIXI.Container();
+
+document.body.appendChild(renderer.view);
+renderer.render(stage);*/
--- js/powerSelect.js
+++ js/powerSelect.js
@@ -7,16 +7,22 @@ window.addEventListener("click", (e) => {
         console.log("vuur");
         break;
     case 2:
+        console.log("water");
         break;
     case 3:
+        console.log("electricity");
         break;
     case 4:
+        console.log("ground");
         break;
     case 5:
+        console.log("wind");
         break;
     case 6:
+        console.log("arcane");
         break;
     default:
+        console.log("But I believe Aang can save the world.");
         break;
     }
 });
