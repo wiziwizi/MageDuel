2c2737424bf5e9395756f84f40182662b2c12730
jvdwijk <joeyvdwijk@gmail.com>
2017-06-22 13:56:18 +0200

    canvas

--- js/drawCanvas.js
+++ js/drawCanvas.js
@@ -3,10 +3,13 @@ let renderer    = PIXI.autoDetectRenderer(
     1080,600,
     {antialias: true, transparent: false, resolution: 1});
 
+let pattenKootje, playerKootje
+
 document.body.appendChild(renderer.view);
 
 renderer.view.style.border = "2px inset black";
 renderer.backgroundColor = 0xffffff;
 
 renderer.render(stage);
+
 //renderer.view.style
