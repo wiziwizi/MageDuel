8f4d3b2c0e3b7cf7b93e23994aaf256d5f04ae70
jvdwijk <joeyvdwijk@gmail.com>
2017-06-26 10:52:39 +0200

    button update

--- js/drawCanvas.js
+++ js/drawCanvas.js
@@ -2,26 +2,31 @@ let stage       = new PIXI.Container();
 let renderer    = PIXI.autoDetectRenderer(
     1080,600,
     {antialias: true, transparent: false, resolution: 1});
+let tink        = new Tink(PIXI, renderer.view);
 
-let tink = new Tink(PIXI, renderer.view);
 
-let buttonImage = new Graphics();
-
+let buttonImage = new PIXI.Graphics();
 buttonImage.beginFill(0x66CCFF);
+buttonImage.lineStyle(4, 0xFF3300, 1);
+buttonImage.drawRect(170, 170, 100, 50);
+buttonImage.endFill();
+tink.makeInteractive(buttonImage);
+buttonImage.press = () => 
+{
+    console.log("Test");
+}
+
+stage.addChild(buttonImage);
 
 document.body.appendChild(renderer.view);
 
 renderer.view.style.border = "2px inset black";
 renderer.backgroundColor = 0xffffff;
 
-
-
 setInterval(loop, 10);
 
-loop()
+function loop()
 {
     tink.update();
     renderer.render(stage);
 }
-
-
