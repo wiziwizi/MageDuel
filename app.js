c163760461af9ceb69e42fb3e292c1a00bda0980
Rico Evers <ricoevers@live.nl>
2017-06-30 20:20:21 +0200

    server fix

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     serverPort = 5000;
 
-let currentPlayer = 0,
+var currentPlayer = 0,
     otherPlayer = 1,
     turn = 0,
     currentTurn = 0;
 
 //The Information of each player
 var playerInfo = {
-    id: 0,
-    element: 0,
+    id: null,
+    element: null,
     battlefase: false,
     health: 100,
     currentAttack: 0
@@ -39,8 +39,8 @@ function dmgCalc(attack, playerElement) {
 
     // attack = +1 in array = strength = less damage
     if (attack == playerElement) return 10;
-    
-    if(playerElement == 4) playerElement = -1;
+
+    if (playerElement == 4) playerElement = -1;
     // attack = -1 in array = weakness = more damage
     if (attack == playerElement + 1) return 50;
     // anything else is neutral damage
@@ -52,10 +52,15 @@ function checkPlayer(socket) {
     for (let i = 0; i < players.length; i++) {
         if (players[i].id == socket.id) {
             currentPlayer = i;
-            otherPlayer = (currentPlayer == 0 ? 1 : 0);
+            otherPlayer = currentPlayer + (currentPlayer % 2 === 0 ? 1 : -1);
             return true;
         }
     }
+}
+
+function displayInfo(socket) {
+    socket.to(players[otherPlayer].id).emit('displayInfo', players[otherPlayer], players[currentPlayer], turn);
+    socket.emit('displayInfo', players[currentPlayer], players[otherPlayer], turn);
 }
 
 //When the player end its turn
@@ -70,7 +75,6 @@ io.on('connection', socket => {
     //adding the player info to the array
     players.push(Object.assign({}, playerInfo));
     players[players.length - 1].id = socket.id;
-    console.log(players);
 
     //Player selects its Power
     socket.on('powerSelect', power => {
@@ -79,11 +83,12 @@ io.on('connection', socket => {
         console.log(players);
         players[currentPlayer].battlefase = true;
         currentTurn = (turn % 2 === 0 ? 0 : 1);
+        displayInfo(socket);
     });
 
     //When he attacks with the element of choice
     socket.on('attack', currentAttack => {
-        
+
         console.log(turn);
         console.log(currentPlayer, otherPlayer);
 
@@ -94,10 +99,9 @@ io.on('connection', socket => {
 
             if (players[otherPlayer].health <= 0) io.sockets.emit('gameOver');
 
+            displayInfo(socket);
             endTurn();
-            io.sockets.emit('displayInfo', players, turn);
         }
-
     });
 
     socket.on('disconnect', () => {
@@ -107,8 +111,9 @@ io.on('connection', socket => {
         }
         if (players.length < 2) turn = 0;
     });
+
     if (players.length < 2) return;
-    io.sockets.emit('displayInfo', players, turn);
+    displayInfo(socket);
 });
 
 server.listen(serverPort);
--- public/socket.js
+++ public/socket.js
@@ -1,12 +1,41 @@
-//var socket = io('https://mage-duel.herokuapp.com:5000/');
+//var socket = io('https://mage-duel.herokuapp.com:5000');
 var socket = io('localhost:5000');
+let message;
 
-socket.on('displayInfo', (players, turn) => {
-    document.getElementById("player1Health").innerHTML = "Player 1 health: " + players[0].health;
+socket.on('displayInfo', (player, enemy, turn) => {
+    
+    if(enemy.health == null) message = "Waiting for enemy to connect";
+    else message = "Enemy player health: " + enemy.health;
+    
+    document.getElementById("player2Health").innerHTML = message;
     document.getElementById("turn").innerHTML = "Turn: " + turn;
 
-    document.getElementById("player2Health").innerHTML = "Player 2 health: " + players[1].health;
-    document.getElementById("turn1").innerHTML = "Turn: " + turn;
+    switch (player.element) {
+        case 0:
+            message = "Player Element: fire";
+            break;
+        case 1:
+            message = "Player Element: water";
+            break;
+        case 2:
+            message = "Player Element: electricity";
+            break;
+        case 3:
+            message = "Player Element: ground";
+            break;
+        case 4:
+            message = "Player Element: wind";
+            break;
+        case 5:
+            message = "Player Element: arcane";
+            break;
+        default:
+            message = "Waiting for enemy to connect";
+            break;
+    }
+
+    document.getElementById("player1Health").innerHTML = "Player health: " + player.health;
+    document.getElementById("element").innerHTML = message;
 });
 
 socket.on('gameOver', () => GameOver());
