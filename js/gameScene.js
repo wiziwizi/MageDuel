536f9ec14a28b275328255224fdb391aab2c0786
jvdwijk <unconfigured@null.spigotmc.org>
2017-06-30 17:07:51 +0200

    small update

--- js/gameScene.js
+++ js/gameScene.js
@@ -62,16 +62,11 @@ buttons[1].press = () => element = 1; //water
 buttons[2].press = () => element = 2; //electricity
 buttons[3].press = () => element = 3; //ground
 buttons[4].press = () => element = 4; //wind
-buttons[5].press = () => //arcane
-    (prepDone ? element = 5 : shake());
+buttons[5].press = () => element = 5; //arcane
 buttons[6].press = () => //ready
     (prepDone ? socket.emit('attack', element) : setElement());
 
 function setElement() {
     socket.emit('powerSelect', element);
     prepDone = true;
-}
-
-function shake() {
-    charm.breathe(buttons[5],3,1.2,20,true, 1);
 }
